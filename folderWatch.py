26674990dddb9731371402dd9edb0e5bb26e65e5
Foster <vtflip@gmail.com>
2020-03-21 10:51:20 -0400

    test

--- folderWatch.py
+++ folderWatch.py
@@ -9,8 +9,7 @@ def main():
 		pass
 	for event in i.event_gen(yield_nones=False):
 		(_,type_names,path,filename)=event
-			
-		 print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(path,filename,type_names))
+	print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(path,filename,type_names))
 	
 if __name__ == '__main__':
 	main()
