ed73564d0ddba63ca4c1761067f783ac14faf6e6
Foster <vtflip@gmail.com>
2020-03-21 10:48:41 -0400

    test

--- folderWatch.py
+++ folderWatch.py
@@ -1,7 +1,17 @@
 import inotify.adapters
 
 def main():
-	print("testsing")
+	i = inotify.adapters.Inotify()
+	
+	i.add_watch('/opt/python_fun/test_watch/')
+	
+	with open('/opt/python_fun/test_watch/test_file','w'):
+		pass
+		for event in i.event_gen(yield_nones=False):
+			(_,type_names,path,filename)=event
+			
+			 print("PATH=[{}] FILENAME=[{}] EVENT_TYPES={}".format(
+              path, filename, type_names))
 	
 if __name__ == '__main__':
 	main()
